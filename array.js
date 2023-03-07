4c1b616f22c9a9f7415520727b5dbd1c466d92c2
FIRST_NAME LAST_NAME <mulianaibrahim@gmail.com>
2023-03-07 21:13:48 +0700

    inisisasi project

---
+++ array.js
@@ -0,0 +1,41 @@
+console.log('tugas1');
+let anggota = ['muliana', 'anisa', 'fahran', 'rayhan', 'indah', 'farah'];
+let bidang = ['adm', 'konsumsi', 'acara', 'dekorasi', 'dokumentasi', 'keamanan'];
+let jurusan = ['sistem informasi', 'teknik informatika', 'teknik elektro', 'teknik mesin', 'teknik kimia'];
+
+console.log(anggota[1]);
+console.log(anggota[3]);
+console.log(anggota[5]);
+console.log(anggota.length)
+
+console.log(bidang[1]);
+console.log(bidang[3]);
+console.log(bidang[5]);
+console.log(bidang.length)
+
+console.log(jurusan[1]);
+console.log(jurusan[3]);
+console.log(jurusan[5]);
+console.log(jurusan.length)
+
+console.log('tugas2');
+let anggota1 = ['muliana', 'anisa', 'fahran', 'rayhan', 'indah', 'farah'];
+let bidang1 = ['adm', 'konsumsi', 'acara', 'dekorasi', 'dokumentasi', 'keamanan'];
+let jurusan1 = ['sistem informasi', 'teknik informatika', 'teknik elektro', 'teknik mesin', 'teknik kimia'];
+
+console.log(anggota1[0]);
+console.log(anggota1[2]);
+console.log(anggota1[4]);
+console.log(anggota1[6]);
+console.log(anggota1.length)
+
+console.log(bidang1[0]);
+console.log(bidang1[2]);
+console.log(bidang1[4]);
+console.log(bidang1[6]);
+console.log(bidang1.length)
+
+console.log(jurusan1[0]);
+console.log(jurusan1[2]);
+console.log(jurusan1[4]);
+console.log(jurusan1.length)
